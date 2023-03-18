186ebcc41d67cb718385506c371aa758f894a498
SamhitaAchanta <sammy.achanta@gmail.com>
2023-03-18 11:52:20 -0500

    add carousel, finish calendar, dynamic form

---
+++ script.js
@@ -0,0 +1,43 @@
+const sr = ScrollReveal ({
+    distance: '80px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+ScrollReveal().reveal('.food-text',{delay:200,origin:'top'});
+ScrollReveal().reveal('.fridge-img',{delay:450,origin:'top'});
+
+
+
+
+// const text = document.querySelector(".title-typewriter");
+// const typeWriter = () => {
+//   setTimeout(() =>{
+//     text.textContent = "breakfast?"
+//   },0);
+
+//   setTimeout(() =>{
+//     text.textContent = "lunch?"
+//   },4000);
+
+//   setTimeout(() =>{
+//     text.textContent = "dinner?"
+//   },8000);
+
+// }
+
+
+// var i = 0;
+// var txt = ["breakfast?","lunch?","dinner?"];
+// var speed = 50;
+
+// function typeWriter() {
+  
+//   if (i < txt.length) {
+//     document.getElementById("title-typewriter").innerHTML += txt[i];
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+
