1c08d52378fb6d18a0e46fe6714d3adb1decbfcf
SamhitaAchanta <sammy.achanta@gmail.com>
2023-03-22 16:56:19 -0500

    added scrollreveal effect on index

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/*
+    Name: Samhita Achanta
+    File Name: script.js
+    Assignment: Project 1
+    This is the main javascript file of the project with all the dynamic functionalities.  
+*/
+
+// Animation for the main section
 const sr = ScrollReveal ({
     distance: '80px',
     duration: 2600,
@@ -8,36 +16,29 @@ const sr = ScrollReveal ({
 ScrollReveal().reveal('.food-text',{delay:200,origin:'top'});
 ScrollReveal().reveal('.fridge-img',{delay:450,origin:'top'});
 
-var 
 
 
-// const text = document.querySelector(".title-typewriter");
-// const typeWriter = () => {
-//   setTimeout(() =>{
-//     text.textContent = "breakfast?"
-//   },0);
+// Insert ingredients dynamic input form
+var options = document.getElementById('options');
+// var add_options = document.getElementById('add_options');
+// var remove_options = document.getElementById('remove_options');
 
-//   setTimeout(() =>{
-//     text.textContent = "lunch?"
-//   },4000);
+function add_options(){
+    var new_option = document.createElement('input');
+    new_option.setAttribute('type','text');
+    new_option.setAttribute('name','text');
+    new_option.setAttribute('class','text');
+    new_option.setAttribute('siz',50);
+    new_option.setAttribute('placeholder','Another Field');
+    options.appendChild(new_option);
+}
 
-//   setTimeout(() =>{
-//     text.textContent = "dinner?"
-//   },8000);
+function remove_options(){
+    var input_tags = options.getElementsByTagName('input');
+    if(input_tags.length > 2){
+        options.removeChild(input_tags[(input_tags.length) - 1]);
+    }
+}
 
-// }
 
-
-// var i = 0;
-// var txt = ["breakfast?","lunch?","dinner?"];
-// var speed = 50;
-
-// function typeWriter() {
-  
-//   if (i < txt.length) {
-//     document.getElementById("title-typewriter").innerHTML += txt[i];
-//     i++;
-//     setTimeout(typeWriter, speed);
-//   }
-// }
 
